0cf40efc60dc2fcf99021a4c355fc96497077d2e
selphiron <a.karakaya@tu-berlin.de>
2021-05-20 18:41:22 +0200

    adds Konstanz

--- src/main/java/Config/Config.java
+++ src/main/java/Config/Config.java
@@ -4,7 +4,7 @@ public class Config {
     // center coordinates for Charlottenburg: 52.51616, 13.31339
     // for Bern: 46.946338, 7.443721
     // for Leipzig: 51.3403333,12.37475
-    public static final String REGIONCENTERCOORDS ="51.3403333, 12.37475";
+    public static final String REGIONCENTERCOORDS ="47.669167, 9.177778";
     // This increases the weight of scary incidents for calculating the dangerousness score
     public static final double SCARINESS_FACTOR = 4.4;
 
@@ -15,16 +15,18 @@ public class Config {
     // BBOX_LATS for Berlin: 52.3319,52.3319,52.6735,52.6735
     // BBOX_LONS for Berlin: 12.9371,13.8034,13.8034,12.9371
     // BBOX_LATS for Leipzig: 51.223668,51.223668,51.446150,51.446150
-    // BBOX_LONS for Leipzig: 12.202565,12.552242,12.552242,112.202565
-    public static final double[] BBOX_LATS = {51.223668,51.223668,51.446150,51.446150};
-    public static final double[] BBOX_LONS = {12.202565,12.552242,12.552242,112.202565};
+    // BBOX_LONS for Leipzig: 12.202565,12.552242,12.552242,12.202565
+    // BBOX_LATS for Konstanz: 47.654161,47.654161,47.941796,47.941796
+    // BBOX_LONS for Konstanz: 8.608894,9.213842,9.213842,8.608894
+    public static final double[] BBOX_LATS = {47.654161,47.654161,47.941796,47.941796};
+    public static final double[] BBOX_LONS = {8.608894,9.213842,9.213842,8.608894};
 
 
     // Paths to the junctions and segments csv files that contain the extracted osm data
-    public static final String JUNCTIONS_PATH = "osm_data/leipzig_junctions.csv";
-    public static final String SEGMENTS_PATH = "osm_data/leipzig_streets.csv";
+    public static final String JUNCTIONS_PATH = "osm_data/konstanz_junctions.csv";
+    public static final String SEGMENTS_PATH = "osm_data/konstanz_streets.csv";
 
-    public static final String GEOJSON_OUTPUT_PATH = "output_data/leipzig.json";
+    public static final String GEOJSON_OUTPUT_PATH = "output_data/konstanz.json";
 
     public static final String DEBUG_PATH = "output_data/debug";
 
